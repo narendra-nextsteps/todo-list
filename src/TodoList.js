540a3d9a798c9cdbe832d5f6b23095413dfbeab8
Narendra Manchala <narendra@Narendras-MacBook-Pro.local>
2018-08-27 12:44:52 +0530

    todo list updated with add

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,12 +1,49 @@
 import React, { Component } from "react";
  
 class TodoList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputVal: '',
+      items: []
+    };
+    this.addItem = this.addItem.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    const value = event.target.value
+
+    this.setState({inputVal: value})
+  }
+
+  addItem(event) {
+
+    if (this.state.inputVal !== "") {
+      var newItem = {
+        text: this.state.inputVal,
+        key: Date.now()
+      };
+   
+      this.setState((prevState) => {
+        return { 
+          items: prevState.items.concat(newItem) 
+        };
+      });
+      this.state.inputVal = "";
+    }
+    console.log(this.state.items);
+    event.preventDefault();  
+ 
+  }
+
   render() {
+  console.log(this.state.items.join())
     return (
       <div className="todoListMain">
         <div className="header">
-          <form>
-            <input placeholder="enter task" />
+          <form onSubmit={this.addItem}>
+            <input placeholder="enter task" value={this.state.inputVal} onChange={this.handleChange}/>
             <button type="submit">add</button>
           </form>
         </div>
